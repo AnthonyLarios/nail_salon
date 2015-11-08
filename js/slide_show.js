7276c67259451005ebe9226001afec2324067078
Anthony Larios <simpsonsruleA52@gmail.com>
2015-11-08 11:44:25 -0800

    Add change function to slide_show.js and Modify styles in index.css

--- js/slide_show.js
+++ js/slide_show.js
@@ -3,6 +3,18 @@ $(function() {
   var timeout;
 
   $slides = $('#slide_show').find('.slide');
+  
+  function change(newIndex) {
+    $currentSlide = $("div#slide_show > img:eq(" + currentIndex + ")");
+    $newSlide = $("div#slide_show > img:eq(" + newIndex + ")");
+    
+    advance();
+
+    $currentSlide.fadeOut(1500).css( {'display':''} ).removeClass('visible').addClass('hidden');
+    $newSlide.fadeIn(1500).css( {'display':''} ).removeClass('hidden').addClass('visible');
+
+    currentIndex = newIndex;
+  }
 
   function advance() {
     clearTimeout(timeout);
